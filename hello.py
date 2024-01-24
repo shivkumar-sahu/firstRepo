81cd727d311d5baf9a0fc730f25be217d9b7ae5e
Shivkumar Sahu <155562316+shivkumar-sahu@users.noreply.github.com>
2024-01-24 20:18:18 +0530

    Update hello.py
    check number

--- hello.py
+++ hello.py
@@ -93,9 +93,16 @@ elif(num2>num3)and(num2>num3):
   print(num2,"is the largest number")
 else:
   print(num3,"is the largest number")
-  
-
-
+  #check prime number
+num=int(input("enter number here:"))
+if num==1:
+  print("it is not a prime number")
+if num>1:
+  for i in range (2,num):
+    if num % ==0:
+      print("it is a prime number")
+else:
+  print("it is a prime number")
 
 
 
