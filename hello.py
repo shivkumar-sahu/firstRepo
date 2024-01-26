4ccdc7c8c021379571c7290185df098e9b9ca1d4
Shivkumar Sahu <155562316+shivkumar-sahu@users.noreply.github.com>
2024-01-26 19:09:54 +0530

    Update hello.py
    prime number h

--- hello.py
+++ hello.py
@@ -109,6 +109,17 @@ import random
 num=random.randint(0,10)
 print(num)
 
+#print all prime numbers
+lower=int(input("enter lower limit here:"))
+upper=int(input("enter upper limit here:"))
+for num in range (lower,upper+1):
+  if num>1:
+    for i in range (2,num):
+      if num%i==0:
+        break
+      else:
+        print(num)
+
 
 
 
