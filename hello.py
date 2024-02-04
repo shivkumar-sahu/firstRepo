f778b354e7d112c81e70126bcb08c6aee4bf8a9f
Shivkumar Sahu <155562316+shivkumar-sahu@users.noreply.github.com>
2024-02-04 20:24:25 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -173,6 +173,15 @@ match x:
         case_:
         print(x)
 
+ #using recursion  factorial no.
+def fact(a):
+  if a==0:
+    return 1
+  else:
+    return((a)*fact(a-1))
+    num=int(input("enter a number here:"))
+    result=fact(num)
+print("the factorial of the given number is ",result)
 
 
 
